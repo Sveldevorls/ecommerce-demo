0c10bbe386f2c8e5a8a22c65d1c6965cd20f98ed
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-04-11 18:25:29 +0800

    Add navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import styles from "./Navbar.module.css"
+import logo from "../../assets/logo.png"
+import cart from "../../assets/cart.svg"
 
-export default function Navbar({ cartItemCount = 0 }) {
+export default function Navbar({ cartItemCount }) {
     return (
-        <nav className={styles.Navbar}>
+        <nav className={styles.navbar}>
+            <NavLink to="/" className={styles.logo}>
+                <img src={logo} alt="Lipsum" />
+            </NavLink>
             <ul>
                 <li>
-                    <Link to="./products">Our products</Link>
+                    <Link to="/products">Our products</Link>
                 </li>
-                <li>About us</li>
-                <li>Help</li>
                 <li>
-                    <Link to="./cart">My cart</Link>
+                    <Link to="#">About us</Link>
+                </li>
+                <li>
+                    <Link to="#">Help</Link>
                 </li>
             </ul>
-            <p className={styles.cart}>
-                {cartItemCount}
-            </p>
+            <div className={styles.cart}>
+                <Link to="/cart">
+                    <img src={cart} className={styles.icon} alt="My cart" />
+                    <span>{cartItemCount < 100 ? cartItemCount : "99+"}</span>
+                </Link>
+            </div>
         </nav>
     )
 }
